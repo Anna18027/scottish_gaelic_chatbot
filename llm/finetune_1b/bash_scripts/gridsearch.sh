b8b2398a45cc8a86ecda2b252411dc3045126a34
Anna McManus <anna.mcmanus99@gmail.com>
2025-07-22 12:19:57 +0100

    partition

--- llm/finetune_1b/bash_scripts/gridsearch.sh
+++ llm/finetune_1b/bash_scripts/gridsearch.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/finetune-%j.out
 #SBATCH --error=logs/finetune-%j.err
 #SBATCH --time=04:00:00
-#SBATCH --partition=Teach-Standard
+#SBATCH --partition=PGR-Standard
 #SBATCH --gres=gpu:gtx_1080_ti:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
